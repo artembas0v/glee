2fdc76f18c6e30fa412968e3c7b02c240e7bae22
artembas0v <artembasov89@gmail.com>
2020-11-25 12:43:58 +0200

    added slider,info,products, fix dz13

--- app/js/main.js
+++ app/js/main.js
@@ -3,9 +3,15 @@ $(function () {
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
-    fade:true,
-    autoplay:true,
-    autoplaySpeed:2000
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+  var mixer = mixitup('.products__content', {
+    selectors: {
+      control: '.products__btn'
+    }
   });
 
 });
